a0351312fcdd7d55d91bbe135f8b53ee3994a3dc
Anas <anas@senangpay.my>
2024-06-12 11:29:34 +0800

    add pupeteer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 const express = require('express')
 const messageRouter = require('./routers/messageRouter')
 const whatsappClient = require('./services/WhatsappClient')
-const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+whatsappClient.initialize()
 
-    const client = new whatsappClient({
-        puppeteer: { browser },
-    });
+const app = express()
 
-    client.on('qr', (qr) => {
-        console.log('QR RECEIVED', qr);
-    });
+app.use(express.json())
+app.use(messageRouter)
 
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
-
-    client.initialize();
-
-    const app = express()
-
-    app.use(express.json())
-    app.use(messageRouter)
-
-    app.listen(3000, () => console.log("server is ready"))
-})();
+app.listen(3000, () => console.log("server is ready"))
