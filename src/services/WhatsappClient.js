7ec007f36d4310515a525ae06ab2d5dfbf9f0a86
Anas <anas@senangpay.my>
2024-07-01 13:54:48 +0800

    changes 1/7/2024

--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -9,7 +9,7 @@ const  whatsappClient = new Client({
     },
     webVersionCache: {
         type: 'remote',
-        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html`
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`
     }
 })
 
