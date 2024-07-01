2421e8a695a137e5dc6bd317cb0f22d8d76b3dba
Anas <anas@senangpay.my>
2024-07-01 15:11:43 +0800

    changed ws version

--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -7,10 +7,11 @@ const  whatsappClient = new Client({
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', ],
     },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: `https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html`
-    }
+    // webVersionCache: {
+    //     type: 'remote',
+    //     remotePath: `https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html`
+    // }
+    webVersion: "2.2412.54v2"
 })
 
 whatsappClient.on("ready", () => console.log("client is ready"))
